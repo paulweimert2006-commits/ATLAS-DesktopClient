5aa434930b0699bec27e690b72710e90da27d4ce
paulweimert2006-commits <paul.weimert.2006@gmail.com>
2026-03-11 19:21:34 +0100

    fix(ui): Adjust animation update interval for smoother transitions in ModuleSidebar
    - Reduced the update interval for enter and exit animations from 32ms to 12ms in the ModuleSidebar component, enhancing the fluidity of UI transitions.

--- src/ui/components/module_sidebar.py
+++ src/ui/components/module_sidebar.py
@@ -304,7 +304,7 @@ class ModuleSidebar(QFrame):
 
         tl = QTimeLine(self._ENTER_DURATION, self)
         tl.setFrameRange(0, 1000)
-        tl.setUpdateInterval(32)
+        tl.setUpdateInterval(12)
         tl.setEasingCurve(QEasingCurve.OutCubic)
         tl.frameChanged.connect(self._apply_enter_frame)
         tl.finished.connect(self._on_enter_finished)
@@ -327,7 +327,7 @@ class ModuleSidebar(QFrame):
 
         tl = QTimeLine(self._EXIT_DURATION, self)
         tl.setFrameRange(0, 1000)
-        tl.setUpdateInterval(32)
+        tl.setUpdateInterval(12)
         tl.setEasingCurve(QEasingCurve.InCubic)
         tl.frameChanged.connect(self._apply_exit_frame)
         tl.finished.connect(self._on_exit_finished)
